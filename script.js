00bc59193a182466631431fc6bef570a4bccdd8a
wizard-hash <aronidengeno@gmail.com>
2025-05-13 10:58:23 +0300

    My Assignment

---
+++ script.js
@@ -0,0 +1,5 @@
+        document.getElementById("Animate").addEventListener(("click"), function(){
+            const theShow = document.getElementById("displayer")
+            theShow.classList.toggle("animate")
+             TheShow.animate.textContent = "helnjijopkkj";
+        })
